11acc2341e7995ce1abbcdf4722e08cfa91a3b7e
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-03-11 15:50:54 -0500

    add create command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(about, long_about = None, version)]
 struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
@@ -9,26 +9,52 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Clean {
-        /// Remove pdf(s)
-        score: String,
-    },
-    Compile {
-        /// Create pdf(s)
-        score: String,
+    /// Remove pdf(s)
+    Clean { score: String },
+
+    /// Create pdf(s)
+    Compile { score: String },
+
+    /// Create new score template
+    Create {
+        #[arg(long)]
+        composer: String,
+
+        #[arg(long)]
+        template: String,
+
+        #[arg(long)]
+        title: String,
+
+        #[arg(long)]
+        edit: bool,
     },
 }
 
 fn main() {
     let cli = Cli::parse();
+
     match &cli.command {
         Some(Commands::Clean { score }) => {
             println!("Removing {}.pdf...", score)
         }
+
         Some(Commands::Compile { score }) => {
             println!("Creating {}.pdf...", score)
         }
+
+        Some(Commands::Create {
+            composer,
+            template,
+            title,
+            edit: _,
+        }) => {
+            println!(
+                "Creating ly file for \"{}\" by {} of type {}...",
+                title, composer, template
+            )
+        }
+
         _ => {
             println!("Please choose a command.")
         }
