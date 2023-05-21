d8848453d2e5ab4ab3e11adea191dd84ac4fc2f2
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-05-21 18:11:58 -0400

    compile only if modified

--- src/commands/compile.rs
+++ src/commands/compile.rs
@@ -1,15 +1,41 @@
 use crate::config::Config;
 use glob::glob;
 use std::fs::create_dir_all;
+use std::fs::metadata;
 use std::io::{self, Write};
 use std::path::PathBuf;
 use std::process::Command;
+use std::time::SystemTime;
+
+fn get_modified(file: &PathBuf) -> Option<SystemTime> {
+    if let Ok(file_metadata) = metadata(file) {
+        Some(file_metadata.modified().unwrap())
+    } else {
+        None
+    }
+}
+
+fn already_compiled(input_file: &PathBuf, output_file: &PathBuf) -> bool {
+    let input_modified = get_modified(input_file);
+    let output_modified = get_modified(output_file);
+
+    input_modified <= output_modified
+}
 
 fn compile_input_file(input_file: &PathBuf, config: &Config) {
     let pdfs_directory = config.pdfs_directory();
     create_dir_all(&pdfs_directory).unwrap();
 
     if let Some(file) = input_file.to_str() {
+        let output_file = PathBuf::from(format!(
+            "{pdfs_directory}/{}.pdf",
+            input_file.file_stem().unwrap().to_str().unwrap()
+        ));
+
+        if already_compiled(input_file, &output_file) {
+            return;
+        }
+
         match Command::new("lilypond")
             .args(["--include", &config.scores_directory()])
             .args(["--output", &pdfs_directory])
