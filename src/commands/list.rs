ab371074ce1ce0deade2a7bd04a8bb71474482f3
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-06-04 14:51:49 -0400

    update list command

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -5,6 +5,7 @@ use std::{
     ffi::OsString,
     fs::{read_dir, DirEntry},
 };
+use titlecase::titlecase;
 
 pub fn list_pdfs(scores: &Vec<String>) {
     let patterns = get_patterns(scores, ".pdf");
@@ -24,37 +25,73 @@ fn get_display(path: &DirEntry) -> String {
     artist.replace("-", " ")
 }
 
-pub fn list_scores(scores: &Vec<String>) {
+struct Composition {
+    artist: String,
+    composition: String,
+}
+
+pub fn list_scores(search_terms: &Vec<String>) {
     let config = Config::new();
     let scores_directory = config.scores_directory();
     let score_files = format!("{scores_directory}/scores");
+    let mut compositions: Vec<Composition> = vec![];
 
     for entry in read_dir(score_files).unwrap() {
         match entry {
             Ok(path) => {
-                let mut lines: Vec<Vec<String>> = vec![];
                 let artist = get_display(&path);
-                // println!("{artist:?}");
 
                 for entry in read_dir(path.path()).unwrap() {
                     let entry = entry.unwrap();
-                    let name = get_display(&entry);
-                    lines.push(vec![artist.clone(), name]);
-                }
+                    let composition = get_display(&entry);
 
-                for line in lines {
-                    println!("    {: <21}    {}", line[0], line[1]);
-                }
+                    let mut is_match = true;
 
-                // match score_name.to_str() {
-                //     Some(test) => {
-                //         println!("{}", test.replace('-', " "));
-                //         list_pdfs(&vec![test.to_string()]);
-                //     }
-                //     None => println!("NO"),
-                // }
+                    if search_terms.len() > 0 {
+                        is_match = false;
+
+                        for term in search_terms {
+                            if artist.contains(term)
+                                || composition.contains(term)
+                            {
+                                is_match = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if is_match {
+                        let path =
+                            String::from(entry.path().to_str().unwrap());
+
+                        // for file in glob(&format!("{path}/*.ly"))
+                        //     .expect("Failed to read glob pattern")
+                        // {
+                        //     match file {
+                        //         Ok(path) => println!("{}", path.display()),
+                        //         Err(message) => println!("{:?}", message),
+                        //     }
+                        // }
+
+                        compositions.push(Composition {
+                            artist: artist.clone(),
+                            composition,
+                        });
+                    }
+                }
             }
             Err(message) => println!("{message}"),
         }
+    }
+
+    if compositions.len() > 0 {
+        println!("    {: <21}    {}", "Artist", "Composition");
+        println!("    {: <21}    {}", "----", "----");
+    }
+
+    for composition in compositions {
+        let artist = titlecase(&composition.artist);
+        let composition = titlecase(&composition.composition);
+        println!("    {artist: <21}    {composition}");
     }
 }
