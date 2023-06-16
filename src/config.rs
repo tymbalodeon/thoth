40eb67a486c640512c4432c98b8ebcb6ab203275
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-06-16 08:31:31 -0400

    update config

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,14 @@ use toml::from_str;
 
 pub static CONFIG_PATH: &str = "~/.config/thoth/config.toml";
 
+fn get_default_scores_directory() -> String {
+    "~/scores".to_string()
+}
+
+fn get_default_pdfs_directory() -> String {
+    "pdfs".to_string()
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Thoth {
     composer: Option<String>,
@@ -12,9 +20,50 @@ struct Thoth {
     pdfs_directory: Option<String>,
 }
 
+impl Default for Thoth {
+    fn default() -> Self {
+        Thoth {
+            composer: Some("".to_string()),
+            scores_directory: Some(get_default_scores_directory()),
+            pdfs_directory: Some(get_default_pdfs_directory()),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct ConfigFile {
     thoth: Option<Thoth>,
+}
+
+impl Default for ConfigFile {
+    fn default() -> Self {
+        ConfigFile {
+            thoth: Some(Thoth::default()),
+        }
+    }
+}
+
+fn load_config_file() -> ConfigFile {
+    let config_path =
+        if let Ok(config_path) = read_to_string(tilde(CONFIG_PATH).as_ref()) {
+            config_path
+        } else {
+            "".to_owned()
+        };
+
+    if let Ok(config_file) = from_str(&config_path) {
+        config_file
+    } else {
+        ConfigFile::default()
+    }
+}
+
+fn get_composer_or_default(thoth: &Thoth) -> String {
+    if let Some(composer) = &thoth.composer {
+        composer.to_string()
+    } else {
+        "".to_string()
+    }
 }
 
 #[derive(Debug)]
@@ -24,64 +73,34 @@ pub struct Config {
     pub pdfs_directory: String,
 }
 
-fn get_default_scores_directory() -> String {
-    "~/scores".to_owned()
-}
-
-fn get_default_pdfs_directory() -> String {
-    "pdfs".to_owned()
-}
-
-fn get_config_path() -> String {
-    let config_path = tilde(CONFIG_PATH);
-
-    if let Ok(result) = read_to_string(config_path.as_ref()) {
-        result
-    } else {
-        "".to_owned()
-    }
-}
-
-fn load_config_file() -> ConfigFile {
-    let config_path = get_config_path();
-
-    if let Ok(config_file) = from_str(&config_path) {
-        config_file
-    } else {
-        ConfigFile {
-            thoth: Some(Thoth {
-                composer: Some("".to_owned()),
-                scores_directory: Some(get_default_scores_directory()),
-                pdfs_directory: Some(get_default_pdfs_directory()),
-            }),
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            composer: "".to_string(),
+            scores_directory: get_default_scores_directory(),
+            pdfs_directory: get_default_pdfs_directory(),
         }
     }
 }
 
 impl Config {
     pub fn new() -> Self {
-        let default_scores_directory = get_default_scores_directory();
-        let default_pdfs_directory = get_default_pdfs_directory();
         let config_file = load_config_file();
 
         if let Some(thoth) = config_file.thoth {
-            let composer = if let Some(composer) = thoth.composer {
-                composer
-            } else {
-                "".to_owned()
-            };
-
+            let composer = get_composer_or_default(&thoth);
             let scores_directory =
                 if let Some(scores_directory) = thoth.scores_directory {
                     scores_directory
                 } else {
-                    default_scores_directory
+                    get_default_scores_directory()
                 };
 
             let pdfs_directory =
                 if let Some(pdfs_directory) = thoth.pdfs_directory {
                     pdfs_directory
                 } else {
+                    let default_pdfs_directory = get_default_pdfs_directory();
                     format!("{scores_directory}/{default_pdfs_directory}")
                 };
 
@@ -91,11 +110,7 @@ impl Config {
                 pdfs_directory,
             }
         } else {
-            Config {
-                composer: "".to_owned(),
-                scores_directory: default_scores_directory,
-                pdfs_directory: default_pdfs_directory,
-            }
+            Config::default()
         }
     }
 
