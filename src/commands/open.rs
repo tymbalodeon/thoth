ac57c972645a865a1a9a50f7ae1cc69e8de55a81
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-06-24 08:24:18 -0400

    add skim to open command

--- src/commands/open.rs
+++ src/commands/open.rs
@@ -1,21 +1,27 @@
 use std::process::Command;
 
-use glob::glob;
+use crate::commands::scores::{get_matching_scores, get_selected_items};
 
-use crate::commands::patterns::get_patterns;
+fn open_file(file_path: &String) {
+    Command::new("open").arg(file_path).output().unwrap();
+    println!("Opened {file_path}");
+}
 
 pub fn open_main(scores: &Vec<String>, pdfs_directory: &Option<String>) {
-    let patterns = get_patterns(scores, ".pdf", &None, pdfs_directory);
+    let matching_scores =
+        get_matching_scores(scores, ".pdf", &None, pdfs_directory);
 
-    for pattern in patterns {
-        for entry in glob(&pattern).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => {
-                    println!("Opening {}...", path.display());
-                    Command::new("open").arg(path).output().unwrap();
-                }
-                Err(message) => println!("{:?}", message),
-            }
+    if matching_scores.len() > 1 {
+        let selected_items = get_selected_items(matching_scores, true);
+
+        for item in selected_items.iter() {
+            let path = item.output().to_string();
+            open_file(&path);
+        }
+    } else {
+        for score in matching_scores {
+            let path = score.to_str().unwrap().to_string();
+            open_file(&path);
         }
     }
 }
