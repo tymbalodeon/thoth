5ae97293602ea738661c38ab6c85d7a2a11a4cd1
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-11-16 21:16:02 -0500

    update lilypond command

--- src/commands/lilypond.rs
+++ src/commands/lilypond.rs
@@ -1,13 +1,10 @@
-use std::fs::read_to_string;
-use std::fs::write;
+use shellexpand::tilde;
+use std::fs::{read_to_string, write};
 
 use super::LilypondCommand;
-use shellexpand::tilde;
-
-static GLOBAL_PATH: &str = "~/.thoth-versions";
 
 fn global(version: &Option<String>) {
-    let global_path = tilde(GLOBAL_PATH).to_string();
+    let global_path = tilde("~/.thoth-versions").to_string();
 
     if let Some(value) = version {
         let _ = write(global_path, value);
