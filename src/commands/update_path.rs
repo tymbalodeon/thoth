ff9c8bb1749d836987437294dcf6a5dde2e4d97b
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-11-23 09:21:57 -0500

    update update path

--- src/commands/update_path.rs
+++ src/commands/update_path.rs
@@ -5,17 +5,7 @@ use crate::commands::lilypond::global::read_global_version;
 
 use super::lilypond::install::{get_install_path, parse_version};
 
-pub fn update_path_main() {
-    let install_path = get_install_path();
-    let global_version = parse_version(&read_global_version());
-    let global_version_path =
-        format!("{}/lilypond-{}/bin", &install_path, &global_version);
-
-    if !Path::new(&global_version_path).exists() {
-        return;
-    }
-
-    let mut path = env::var("PATH").unwrap();
+fn clear_lilypond(mut path: String) -> String {
     let values: &Vec<String> = &path
         .split(':')
         .filter(|value| value.contains("lilypond"))
@@ -26,10 +16,26 @@ pub fn update_path_main() {
         path = path.replace(&format!("{value}:"), "");
     }
 
+    path
+}
+
+pub fn update_path_main() {
+    let install_path = get_install_path();
+    let global_version = parse_version(&read_global_version());
+    let global_version_path =
+        format!("{}/lilypond-{}/bin", &install_path, &global_version);
+    let path = env::var("PATH").unwrap();
+
+    if !Path::new(&global_version_path).exists()
+        || path.contains(&global_version_path)
+    {
+        return;
+    }
+
     let mut new_path = String::new();
 
     new_path.push_str(&format!("{}:", global_version_path));
-    new_path.push_str(&path);
+    new_path.push_str(&clear_lilypond(path));
     new_path = new_path.replace(' ', "\\ ");
 
     let path_command = format!("PATH={new_path}");
