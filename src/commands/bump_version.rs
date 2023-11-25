b18c20e40795da663dbf0bd444059a1db2b3420c
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-11-25 18:06:15 -0500

    update bump version

--- src/commands/bump_version.rs
+++ src/commands/bump_version.rs
@@ -1,5 +1,8 @@
+use std::cmp::Ordering;
 use std::fs::{rename, File};
 use std::io::{BufRead, BufReader, Write as IoWrite};
+
+use human_sort::compare;
 
 use super::lilypond::global::get_global_version;
 use super::scores::get_found_ly_files;
@@ -12,13 +15,16 @@ fn get_new_version(version: &Option<String>) -> String {
     }
 }
 
+fn is_outdated(version: &String, new_version: &String) -> bool {
+    compare(version, new_version) == Ordering::Less
+}
+
 pub fn bump_version_main(
     search_terms: &Vec<String>,
     version: &Option<String>,
     scores_directory: &Option<String>,
 ) {
     let new_version = get_new_version(version);
-    dbg!(&new_version);
     let ly_files =
         get_found_ly_files(search_terms, &false, &false, scores_directory);
 
@@ -31,8 +37,19 @@ pub fn bump_version_main(
             let mut line = line.unwrap();
 
             if line.contains("\\version") {
-                let version = line.replace("\\version ", "").replace('"', "");
-                line = line.replace(&version, new_version.as_str());
+                let file_version =
+                    line.replace("\\version ", "").replace('"', "");
+
+                if version.is_some()
+                    || is_outdated(&file_version, &new_version)
+                {
+                    line = line.replace(&file_version, new_version.as_str());
+                    println!(
+                        "Updated {} to lilypond {}",
+                        &file.display(),
+                        &new_version
+                    );
+                }
             }
 
             line = format!("{line}\n");
