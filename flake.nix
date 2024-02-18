083224d68c63f9c66b4b9228d2572f49d98f09d1
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-02-18 16:01:34 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
@@ -46,62 +45,83 @@
         with pkgs;
         let
           craneLib = crane.lib.${system};
-          allPackages = [ libiconv ];
 
-          darwinPackages = [
+          buildPackages = [
+            libiconv
+          ];
+
+          darwinBuildPackages = [
             zlib.dev
             darwin.apple_sdk.frameworks.CoreFoundation
             darwin.apple_sdk.frameworks.CoreServices
             darwin.apple_sdk.frameworks.SystemConfiguration
             darwin.IOKit
           ];
+
+          linuxBuildPackages = [
+            pkg-config
+            openssl
+          ];
         in
         {
           default = craneLib.buildPackage {
             src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-            buildInputs =
+            buildInputs = buildPackages ++ (
               if system == "x86_64-darwin"
-              then allPackages ++ darwinPackages
-              else allPackages;
+              then darwinBuildPackages
+              else linuxBuildPackages
+            );
           };
         });
 
-      devShells = forEachSupportedSystem ({ pkgs, system }: {
-        default = pkgs.mkShell {
-          packages = with pkgs;
-            let
-              allPackages = [
-                rustToolchain
-                cargo-bloat
-                cargo-edit
-                cargo-outdated
-                cargo-udeps
-                cargo-watch
-                rust-analyzer
-                git
-                just
-                python311Packages.pre-commit-hooks
-                nixpkgs-fmt
-                libiconv
-              ];
+      devShells = forEachSupportedSystem ({ pkgs, system }:
+        with pkgs;
+        let
+          buildPackages = [
+            libiconv
+          ];
 
-              darwinPackages = [
-                zlib.dev
-                darwin.apple_sdk.frameworks.CoreFoundation
-                darwin.apple_sdk.frameworks.CoreServices
-                darwin.apple_sdk.frameworks.SystemConfiguration
-                darwin.IOKit
-              ];
-            in
-            if system == "x86_64-darwin"
-            then allPackages ++ darwinPackages
-            else allPackages;
+          darwinBuildPackages = [
+            zlib.dev
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+            darwin.apple_sdk.frameworks.SystemConfiguration
+            darwin.IOKit
+          ];
 
-          env = {
-            RUST_BACKTRACE = "1";
+          linuxBuildPackages = [
+            pkg-config
+            openssl
+          ];
+
+          devPackages = [
+            rustToolchain
+            cargo-bloat
+            cargo-edit
+            cargo-outdated
+            cargo-udeps
+            cargo-watch
+            rust-analyzer
+            git
+            just
+            python311Packages.pre-commit-hooks
+            nixpkgs-fmt
+            nil
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = buildPackages ++ devPackages ++ (
+              if system == "x86_64-darwin"
+              then darwinBuildPackages
+              else linuxBuildPackages
+            );
+
+            env = {
+              RUST_BACKTRACE = "1";
+            };
           };
-        };
-      });
+        });
     };
 }
