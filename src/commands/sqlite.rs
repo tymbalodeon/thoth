0a7ad7b17bd82a2d2b57ac7952c37e3dbbaa0e50
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-03-25 17:26:18 -0400

    add crude title and composer

--- src/commands/sqlite.rs
+++ src/commands/sqlite.rs
@@ -1,3 +1,5 @@
+use std::fs::read_to_string;
+
 use diesel::{
     insert_into, Connection, QueryDsl, RunQueryDsl, SelectableHelper,
     SqliteConnection,
@@ -5,6 +7,7 @@ use diesel::{
 use diesel_migrations::{
     embed_migrations, EmbeddedMigrations, MigrationHarness,
 };
+use regex::Regex;
 use rust_search::SearchBuilder;
 use shellexpand::tilde;
 
@@ -23,12 +26,16 @@ fn run_migrations(
 fn insert_score(
     connection: &mut SqliteConnection,
     ly_file_path: &String,
+    title: Option<String>,
+    composer: Option<String>,
 ) -> Score {
     use crate::schema::scores;
 
     insert_into(scores::table)
         .values(&NewScore {
-            ly_file_path,
+            ly_file_path: ly_file_path.to_string(),
+            title,
+            composer,
             ..NewScore::default()
         })
         .returning(Score::as_returning())
@@ -51,6 +58,49 @@ fn show_scores(connection: &mut SqliteConnection) {
     }
 }
 
+fn get_title(path: &str) -> Option<String> {
+    let regex = Regex::new(r"title\s*=\s*.*").unwrap();
+
+    if let Ok(contents) = read_to_string(path) {
+        if let Some(result) = regex.find(&contents) {
+            Some(
+                result
+                    .as_str()
+                    .split("=")
+                    .last()
+                    .unwrap()
+                    .trim()
+                    .replace('"', ""),
+            )
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn get_composer(path: &str) -> Option<String> {
+    let regex = Regex::new(r"composer\s*=\s*.*").unwrap();
+
+    if let Ok(contents) = read_to_string(path) {
+        if let Some(result) = regex.find(&contents) {
+            Some(
+                result
+                    .as_str()
+                    .split("=")
+                    .last()
+                    .unwrap()
+                    .trim()
+                    .replace('"', ""),
+            )
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
 pub fn main(import: &bool) {
     let database_url = tilde("~/.local/share/thoth/db.sqlite");
 
@@ -67,7 +117,10 @@ pub fn main(import: &bool) {
             .collect();
 
         for path in search {
-            let score = insert_score(connection, &path);
+            let title = get_title(&path);
+            let composer = get_composer(&path);
+            let score = insert_score(connection, &path, title, composer);
+
             println!("{score:?}");
         }
     }
--- src/models.rs
+++ src/models.rs
@@ -35,21 +35,21 @@ pub struct FileLink {
 
 #[derive(Default, Insertable)]
 #[diesel(table_name = scores)]
-pub struct NewScore<'a> {
-    pub dedication: Option<&'a str>,
-    pub title: Option<&'a str>,
-    pub subtitle: Option<&'a str>,
-    pub subsubtitle: Option<&'a str>,
-    pub instrument: Option<&'a str>,
-    pub poet: Option<&'a str>,
-    pub composer: Option<&'a str>,
-    pub meter: Option<&'a str>,
-    pub arranger: Option<&'a str>,
-    pub tagline: Option<&'a str>,
-    pub copyright: Option<&'a str>,
-    pub piece: Option<&'a str>,
-    pub opus: Option<&'a str>,
-    pub ly_file_path: &'a str,
+pub struct NewScore {
+    pub dedication: Option<String>,
+    pub title: Option<String>,
+    pub subtitle: Option<String>,
+    pub subsubtitle: Option<String>,
+    pub instrument: Option<String>,
+    pub poet: Option<String>,
+    pub composer: Option<String>,
+    pub meter: Option<String>,
+    pub arranger: Option<String>,
+    pub tagline: Option<String>,
+    pub copyright: Option<String>,
+    pub piece: Option<String>,
+    pub opus: Option<String>,
+    pub ly_file_path: String,
 }
 
 #[derive(Debug, Queryable, Selectable)]
